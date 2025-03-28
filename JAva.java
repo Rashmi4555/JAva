1359eac03fc4baa9ad159b3f1d74a1addd57e968
Rashmi4555 <rashmibm3579@gmail.com>
2025-03-28 10:54:17 +0530

    Savethefile

--- JAva.java
+++ JAva.java
@@ -1,6 +1,5 @@
 public class JAva
 {
     public static void main(String[]args){
-        System.out.println("Hello World");
-    } 
-}
+        System.out.println("Hello World this is java");
+    } }
